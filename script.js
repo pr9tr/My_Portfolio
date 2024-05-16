1c29ce986dd4c3b7bda623ef5e94cb9ebf995c26
Pranav <pranavtiwary738@gmail.com>
2024-05-16 23:54:17 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+function triggerDownload() {
+  const btn = document.getElementById("btn-downloadp");
+  const loadingBar = document.getElementById("loading-bar");
+  const content = document.getElementById("content");
+  content.innerHTML = ""
+  btn.classList.add("toLoadingBar");
+  loadingBar.classList.add("isLoading");
+
+  setTimeout(() => {
+    loadingBar.classList.add("isLoading");
+    setTimeout(() => {
+      loadingBar.style.display = 'none';
+      btn.innerHTML = 'Downloaded!';
+      btn.classList.add("toButton");
+      window.open('./assets/resume-example.pdf');
+    }, 1000)
+  }, 300);
+}
